45cb317155ee7d55f2a8604ffc808ed03edaf132
999tommy <adegboyetommy@gmail.com>
2025-03-28 15:33:53 +0100

    readded ts statements

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,5 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/', // Adjust this if your app is hosted in a subdirectory
   plugins: [react(), tailwindcss()],
 });
